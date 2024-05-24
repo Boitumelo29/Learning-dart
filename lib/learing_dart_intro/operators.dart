a8af421b8a062c22466e748dd7f4b4ac8b66a3e4
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-24 12:15:27 +0200

    update

---
+++ lib/learing_dart_intro/operators.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+//Operators:
+//They are used to perform mathematical and logical operations on the variable
+//operands: it represent the data
+//operator: represents how the operands will be processed to produce a value
+
+//Types:
+//-Arithmetic operators:
+//-Increment and decrement
+//-Assignment Operators
+//-Logical Operators
+//-Type Test Operators
+class Operators {}
